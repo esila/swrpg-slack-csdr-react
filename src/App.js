76e3c55e84998f27aa167169b18c0792c4704463
Eckersid Silapaswang <esilapswang@advancelocal.net>
2020-09-17 20:13:03 -0400

    Initial cut at some messaging with graphql

--- src/App.js
+++ src/App.js
@@ -1,19 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import { API, Auth } from 'aws-amplify';
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
+import { listMessages } from './graphql/queries';
+import { createMessage as createMessageMutation } from "./graphql/mutations";
 import './App.css';
-import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
 
+const initialFormState = { message: "", };
 
 function App() {
-  return (
+    const [user, setUser] = useState("");
+    const [messages, setMessages] = useState([]);
+    const [formData, setFormData] = useState(initialFormState);
+
+    useEffect(() => {
+        fetchMessages();
+        Auth.currentAuthenticatedUser({
+            bypassCache: false
+        }).then(user => {
+            console.log(`USER: ${user.username}`);
+            setUser(user.username);
+            setFormData({...formData, user: user.username});
+        });
+    }, []);
+
+    async function fetchMessages() {
+        const apiData = await API.graphql({ query: listMessages});
+        setMessages(apiData.data.listMessages.items);
+    }
+
+    async function createMessage() {
+        if (!formData.message || !formData.user) return;
+        await API.graphql({ query: createMessageMutation, variables: { input: formData } });
+        setMessages([ ...messages, formData ]);
+        setFormData({...initialFormState, user: user});
+    }
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>We now have Auth!</h1>
-      </header>
+        <div>
+            {messages.map((message, idx) => (
+                <div key={idx}>
+                    <p><strong>{message.user}:</strong>  {message.message}</p>
+                </div>
+            ))}
+        </div>
+        <form onSubmit={(event) => {
+            event.preventDefault();
+            //console.log(`${formData.user}: ${formData.message}`);
+            createMessage()
+        }}>
+            <input
+                style={{marginTop: "30px", marginBottom: "30px"}}
+                onChange={e => setFormData({ ...formData, 'message': e.target.value})}
+                placeholder=""
+                value={formData.message}
+            />
+        </form>
       <AmplifySignOut/>
     </div>
-  );
+    );
 }
 
 export default withAuthenticator(App);
--- src/aws-exports.js
+++ src/aws-exports.js
@@ -7,7 +7,11 @@ const awsmobile = {
     "aws_cognito_region": "us-east-1",
     "aws_user_pools_id": "us-east-1_GxmGSnsCG",
     "aws_user_pools_web_client_id": "7cdet801ec8tms7lispog4cmmn",
-    "oauth": {}
+    "oauth": {},
+    "aws_appsync_graphqlEndpoint": "https://bejoyzjlhvgxpbhs6iim6fviqq.appsync-api.us-east-1.amazonaws.com/graphql",
+    "aws_appsync_region": "us-east-1",
+    "aws_appsync_authenticationType": "API_KEY",
+    "aws_appsync_apiKey": "da2-bjnn24dbrzbx3hcbxo7y6e3m2m"
 };
 
 
