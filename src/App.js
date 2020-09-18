455461fb2216d19acd52c7343731f7046c39554f
Eckersid Silapaswang <esilapswang@advancelocal.net>
2020-09-18 13:50:54 -0400

    User context passed and working

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
 import Chat from './Chat';
 import './App.css';
 
+const UserContext = React.createContext("");
+
 function App() {
     const [user, setUser] = useState("");
 
@@ -16,14 +18,19 @@ function App() {
         });
     }, []);
 
-    return (
-        <div className="App">
-            <div className="app__body">
-                <Chat user={user}/>
+    return user ? (
+        <UserContext.Provider value={user}>
+            <div className="App">
+                <div className="app__body">
+                    <Chat userContext={UserContext}/>
+                    <AmplifySignOut/>
+                </div>
             </div>
-            <AmplifySignOut/>
-        </div>
-    );
+        </UserContext.Provider>
+    )
+        :
+        <div></div>
+
 }
 
 export default withAuthenticator(App);
