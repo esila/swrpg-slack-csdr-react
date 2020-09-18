144ca413e4359faca987066abe559a7e08cf95da
Eckersid Silapaswang <esilapswang@advancelocal.net>
2020-09-17 20:14:18 -0400

    graphql stuff

---
+++ src/graphql/subscriptions.js
@@ -0,0 +1,42 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onCreateMessage = /* GraphQL */ `
+  subscription OnCreateMessage {
+    onCreateMessage {
+      id
+      message
+      timestamp
+      user
+      userImage
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onUpdateMessage = /* GraphQL */ `
+  subscription OnUpdateMessage {
+    onUpdateMessage {
+      id
+      message
+      timestamp
+      user
+      userImage
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onDeleteMessage = /* GraphQL */ `
+  subscription OnDeleteMessage {
+    onDeleteMessage {
+      id
+      message
+      timestamp
+      user
+      userImage
+      createdAt
+      updatedAt
+    }
+  }
+`;
